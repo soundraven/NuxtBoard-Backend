21ca1413cccbcd9d5a38caa55d4117973f5efd1a
soundraven <kin9709@gmail.com>
2024-06-25 16:56:46 +0900

    환경변수에 DB_PORT 추가

--- api/config/dbPool.ts
+++ api/config/dbPool.ts
@@ -5,6 +5,7 @@ dotenv.config()
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "3306"),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
