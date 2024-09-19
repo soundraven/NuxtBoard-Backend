4456f86633c722ccfddd1cb0390889a1a0d2d988
soundraven <kin9709@gmail.com>
2024-09-19 23:46:59 +0900

    대소문자 문제 해결을 위한 임시 커밋

--- api/config/corsOptions.ts
+++ api/config/corsOptions.ts
@@ -1,5 +1,17 @@
+const allowedOrigins = [
+  "https://nuxt-board-frontend.vercel.app",
+  "http://localhost:3000",
+]
+
 const corsOptions = {
-  origin: "https://nuxt-board-frontend.vercel.app",
+  origin: function (origin: any, callback: any) {
+    // 요청의 origin이 허용된 목록에 있는지 확인
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true) // 허용된 도메인일 경우
+    } else {
+      callback(new Error("Not allowed by CORS")) // 허용되지 않은 도메인일 경우
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "requiresToken"],
 }
