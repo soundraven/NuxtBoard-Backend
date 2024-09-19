d88e0592e95261f293ab93226810168a5d5c40c1
soundraven <kin9709@gmail.com>
2024-09-20 00:29:19 +0900

    CORS 에러 해결을 위한 corsOptions.ts 파일 수정

--- api/config/corsOptions.ts
+++ api/config/corsOptions.ts
@@ -5,7 +5,6 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin: any, callback: any) {
-    // 요청의 origin이 허용된 목록에 있는지 확인
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true) // 허용된 도메인일 경우
     } else {
@@ -14,6 +13,8 @@ const corsOptions = {
   },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "requiresToken"],
+  preflightContinue: false, // 프리플라이트 요청에서 다음 미들웨어로 가지 않도록 설정
+  optionsSuccessStatus: 204, // 프리플라이트 요청이 성공할 경우 상태 코드 설정
 }
 
 export default corsOptions
