e51f981db2b439379522577217006becf82d5df5
soundraven <kin9709@gmail.com>
2024-09-19 22:55:57 +0900

    cors 옵션 파일 수정, 프론트엔드 도메인에서만 허용

--- api/config/corsOptions.ts
+++ api/config/corsOptions.ts
@@ -1,7 +1,7 @@
 const corsOptions = {
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "requiresToken"],
+  origin: "https://nuxt-board-frontend.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "requiresToken"],
 }
 
 export default corsOptions
